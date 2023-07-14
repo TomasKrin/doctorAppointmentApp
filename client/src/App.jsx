b39db5cb09e7a64004efd6453c142a5d7f79c914
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-07-14 23:18:48 +0300

    installed MUI and implemented localizer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
+import "moment/locale/lt";
+
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import Routes from "./routes/Routes";
 
 const App = () => {
-  return <Routes />;
+  return (
+    <>
+      <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="lt">
+        <Routes />
+      </LocalizationProvider>
+    </>
+  );
 };
 
 export default App;
