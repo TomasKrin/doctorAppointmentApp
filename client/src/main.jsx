a255a32acc40e8c05542d6796b2391d24c6557b5
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-07-15 15:56:02 +0300

    .

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
@@ -12,7 +12,7 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <QueryClientProvider client={queryClient}>
         <App />
         <Toaster
@@ -28,6 +28,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           }}
         />
       </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
